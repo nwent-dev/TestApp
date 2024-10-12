a50541d72f8700be90c4d8823686a89b5e85f1a9
nwent-dev <nwent@proton.me>
2024-10-12 14:42:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-// Указываем папку для статических файлов (например, папка с index.html)
-// app.use(express.static(path.join(__dirname, 'public')));
 
 // Настройка редиректа
 app.get('/', (req, res) => {
     // Условие для редиректа
-    console.log('Redirect condition:', redirectCondition); // Логируем значе
     const redirectCondition = true; // Измени это условие в зависимости от логики
     if (redirectCondition) {
         res.redirect('https://ya.ru'); // Перенаправляем на ya.ru
     } else {
-        res.redirect('/index.html'); // Перенаправляем на index.html
+        res.redirect('https://твой-домен.com'); // Перенаправляем на твой сайт
     }
 });
 
