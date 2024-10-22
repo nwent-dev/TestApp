fdb8ec96dcb9d29aa36a80b9ad70d9efeaddd156
nwent-dev <nwent@proton.me>
2024-10-22 16:00:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 const app = express();
+
+const targetUrl = 'https://opedunkes.beget.app/K5TrzQ';
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Настройка редиректа
-app.get('/redirect', (req, res) => {
-    // Условие для редиректа
-    const redirectCondition = false; // Измени это условие в зависимости от логики
-    if (redirectCondition) {
-        res.redirect('https://yandex.ru'); // Перенаправляем на ya.ru
-    } else {
-        res.redirect('https://opedunkes.beget.app/K5TrzQ'); // Перенаправляем на твой сайт
+app.get('/redirect', async (req, res) => {
+  try {
+    // Делаем запрос к внешнему сайту
+    const response = await axios.get(targetUrl);
+
+    // Если статус ответа не 404, перенаправляем пользователя
+    if (response.status !== 404) {
+      return res.redirect(targetUrl);
     }
+  } catch (error) {
+    // Если произошла ошибка (например, 404), делаем другой редирект
+    if (error.response && error.response.status === 404) {
+      return res.redirect('/index.html');
+    }
+    // Обработка других ошибок
+    return res.status(500).send('Произошла ошибка');
+  }
 });
 
-app.use((req, res) => {
-  // В случае 404 перенаправляем на другой сайт
-  res.redirect('/index.html');
-});
+// Настройка редиректа
+
 
 // Запуск сервера
 const port = process.env.PORT || 3000;
